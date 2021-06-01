73890860a29eb083c360dcc2bf4889195715f9b6
Imam <imamtrywibowo257@gmail.com>
2021-06-01 15:14:17 +0700

    add config to heroku

--- upsharing/settings.py
+++ upsharing/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
